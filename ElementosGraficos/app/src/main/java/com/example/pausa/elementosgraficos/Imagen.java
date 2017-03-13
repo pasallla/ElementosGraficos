3407021cc694af58162894702631476ece653bfe
pausa <pasallla@epsg.upv.es>
2017-03-13 22:49:10 +0100

    Proyecto final

--- ElementosGraficos/app/src/main/java/com/example/pausa/elementosgraficos/Imagen.java
+++ ElementosGraficos/app/src/main/java/com/example/pausa/elementosgraficos/Imagen.java
@@ -1,15 +1,10 @@
 package com.example.pausa.elementosgraficos;
 
-import android.Manifest;
-import android.content.ContentValues;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
-import android.support.v4.content.ContextCompat;
-import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -30,37 +25,37 @@ public class Imagen extends AppCompatActivity {
 
     private ImageView image;
 
-    Uri mPhotoUri;
     Uri fileUri;
 
-    public static final int MEDIA_TYPE_IMAGE = 1;
-    public static final int MEDIA_TYPE_VIDEO = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_imagen);
 
+        //Enlazamos el ImageView creado en xml con image y lo hacemos invisible por el momento
         image = (ImageView) findViewById(R.id.image);
         image.setVisibility(View.INVISIBLE);
 
     }
 
+    //onClick boton Seleccionar Imagen
     public void buscarImagen(View v) {
+
+        //Abrimos galeria para escoger una imagen
         Intent intent = new Intent();
         intent.setType("image/*");
         intent.setAction(Intent.ACTION_GET_CONTENT);
         startActivityForResult(Intent.createChooser(intent, "Select Picture"), SELECT_IMAGE);
     }
 
+    //onClick boton Hacer Foto
     public void hacerFoto(View v) {
 
-        //ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
         Intent intent = new Intent();
         intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
-        //mPhotoUri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, new ContentValues());
-        try {
-            fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
+       try {
+            fileUri = obtenerUriImagen();
             Log.d("Pau.ElementosGraficos","fileUri: " + fileUri);
         } catch (Exception ex) {
             Log.e("Pau.ElementosGraficos", ex.toString());
@@ -73,24 +68,16 @@ public class Imagen extends AppCompatActivity {
     /**
      * Create a file Uri for saving an image or video
      */
-    private static Uri getOutputMediaFileUri(int type) {
-        return Uri.fromFile(getOutputMediaFile(type));
+    private static Uri obtenerUriImagen() {
+        return Uri.fromFile(obtenerFicheroImagen());
     }
 
-    private static File getOutputMediaFile(int type) {
-        // To be safe, you should check that the SDCard is mounted
-        // using Environment.getExternalStorageState() before doing this.
+    private static File obtenerFicheroImagen() {
 
-        File mediaStorageDir = null;
-        mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_PICTURES), "MyCameraApp");
-        // This location works best if you want the created images to be shared
-        // between applications and persist after your app has been uninstalled.
+        //Directorio donde creara el fichero
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "MyCameraApp");
 
-        Log.d("Pau.ElementosGraficos", "getExternalStorage: "+Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).toString());
-        Log.d("Pau.ElementosGraficos", "mediaStorageDir: "+mediaStorageDir.toString());
-
-        // Create the storage directory if it does not exist
+        // Crear la carpeta si no existe
         if (!mediaStorageDir.exists()) {
             if (!mediaStorageDir.mkdirs()) {
                 Log.d("Pau.ElementosGraficos", "failed to create directory");
@@ -98,62 +85,44 @@ public class Imagen extends AppCompatActivity {
             }
         }
 
-        // Create a media file name
+        // Crear el fichero
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        File mediaFile;
-        if (type == MEDIA_TYPE_IMAGE) {
-            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
-                    "IMG_" + timeStamp + ".jpg");
-            Log.e("Pau.ElementosGraficos", "type=MEDIA_TYPE_IMAGE");
-        } else if (type == MEDIA_TYPE_VIDEO) {
-            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
-                    "VID_" + timeStamp + ".mp4");
-            Log.e("Pau.ElementosGraficos", "type=MEDIA_TYPE_VIDEO");
-        } else {
-            Log.e("Pau.ElementosGraficos", "type=(null)");
-            return null;
-        }
+
+        File mediaFile = new File(mediaStorageDir.getPath() + File.separator + "IMG_" + timeStamp + ".jpg");
 
         return mediaFile;
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent respuesta) {
-        Log.e("Pau.Actividades", "dentro de onActivityResult");
+        Log.e("Pau.ElementosGraficos", "dentro de onActivityResult");
         Uri ruta = null;
 
-        Log.d("Pau.ElementosGraficos", "SELECT_IMAGE: "+SELECT_IMAGE);
-        Log.d("Pau.ElementosGraficos", "TAKE_IMAGE: "+TAKE_IMAGE);
-        Log.d("Pau.ElementosGraficos", "RESULT_OK: "+RESULT_OK);
-        Log.d("Pau.ElementosGraficos", "requestCode: "+requestCode);
-        Log.d("Pau.ElementosGraficos", "resultCode: "+resultCode);
-        Log.d("Pau.ElementosGraficos", "respuesta: "+respuesta);
-
-        if (requestCode == SELECT_IMAGE && resultCode == RESULT_OK && respuesta != null) {
-            ruta = respuesta.getData();
-        } else if (requestCode == TAKE_IMAGE && resultCode == RESULT_OK) {
-            ruta = fileUri;
-        } else {
-            Toast.makeText(this, "Algo no ha ido bien", Toast.LENGTH_SHORT).show();
+        if (requestCode == SELECT_IMAGE && resultCode == RESULT_OK && respuesta != null) { // Intent de Seleccionar Imagen
+            ruta = respuesta.getData(); //Obtenemos la ruta del intent
+        } else if (requestCode == TAKE_IMAGE && resultCode == RESULT_OK) { //Intent de Hacer Foto
+            ruta = fileUri; //Obtenemos la ruta del fichero que hemos creado antes de activar el Intent
+        } else { // Si no es ninguno de los anteriores
+            Log.d("Pau.ElementosGraficos", "Algo no ha ido bien al recibir los Intents");
         }
+
+        // Comprobamos que la ruta tenga un Uri
         if (ruta != null) {
             Toast.makeText(this, "Tengo la imagen: " + ruta, Toast.LENGTH_SHORT).show();
-            Bitmap bitmap;
-            try {
-                bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), ruta);
-                image.setImageBitmap(bitmap);
-                image.setVisibility(View.VISIBLE);
-            } catch (IOException ex) {
-                Toast.makeText(this, "Estoy en el catch", Toast.LENGTH_SHORT).show();
-            }
 
+            // Ponemos la imagen en le ImageView
+            ponerImagen(ruta, image);
+
+            //Enviamos la foto por email
             enviarFotoPorEmail(ruta,new String[]{"pasallla@epsg.upv.es"},"Envio de foto por email","Esto es el texto");
         } else {
-            Toast.makeText(this, "ruta = null !!!!!", Toast.LENGTH_SHORT).show();
+            Log.e("Pau.ElementosGraficos", "ruta = null !!!!!");
         }
     }
 
+    // Enviamos por email una foto
     private void enviarFotoPorEmail(Uri foto, String[] emails, String asunto, String texto){
+        Log.d("Pau.ElementosGraficos","foto a enviar: " + foto.toString());
         Intent i = new Intent(Intent.ACTION_SEND);
         i.setType("image/jpeg");
         i.putExtra(Intent.EXTRA_STREAM, foto);
@@ -162,6 +131,25 @@ public class Imagen extends AppCompatActivity {
         i.putExtra(Intent.EXTRA_SUBJECT, asunto);
         i.putExtra(Intent.EXTRA_TEXT, texto);
 
+        startActivity(i);
+
+    }
+
+    // Modificamos el ImageView para que muestre la imagen que obtenemos del Uri
+    private void ponerImagen(Uri ruta, ImageView i){
+
+        try {
+            Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), ruta);
+            ponerImagen(bitmap,i);
+        } catch (IOException ex) {
+            Toast.makeText(this, "Estoy en el catch", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    // Modificamos el ImageView para que muestre la imagen que obtenemos del Bitmap
+    private void ponerImagen (Bitmap b, ImageView i){
+        i.setImageBitmap(b);
+        i.setVisibility(View.VISIBLE);
     }
 
 }
